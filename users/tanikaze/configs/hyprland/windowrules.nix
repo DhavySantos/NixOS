bfb98d8748739c16a37001d5ccd50c20b7df8b7d
Dhavy Santos <dhavy.s@proton.me>
2024-09-21 02:10:34 -0300

    init: [tanikaze] create hyprland windowrules configuration

---
+++ users/tanikaze/configs/hyprland/windowrules.nix
@@ -0,0 +1,14 @@
+[
+	"workspace 10,class:^steam_app_(.*)$"
+	"fullscreen,class:^steam_apps_(.*)$"
+	"monitor 1,class:^steam_apps_(.*)$"
+
+	"workspace special:magic, class:vesktop"
+	"suppressevent maximize, class:.*"
+	"float,class:pulsemixer"
+	"float,class:nmtui"
+
+	"workspace 10,class:gamescope"
+	"fullscreen,class:gamescope"
+	"monitor 1,class:gamescope"
+]
