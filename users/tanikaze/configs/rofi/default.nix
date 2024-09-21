d9dfdbadd1c94948beed47491d8df7307bd70759
Dhavy Santos <dhavy.s@proton.me>
2024-09-21 02:04:51 -0300

    init: [tanikaze] create rofi configuration

---
+++ users/tanikaze/configs/rofi/default.nix
@@ -0,0 +1,37 @@
+{ lib, config, ... } :
+
+let
+
+colors = with config.lib.stylix.colors; ''
+* {
+	background:     #${base01}FF;
+	background-alt: #${base02}FF;
+	foreground:     #${base05}FF;
+	selected:       #${base07}FF;
+	active:         #${base0C}FF;
+	urgent:         #${base08}FF;
+}
+'';
+
+style = builtins.readFile ./style.rasi;
+
+in
+
+{
+	programs.rofi.enable = true;
+	programs.rofi.extraConfig = {
+		modi = "drun";
+		show-icons = true;
+		display-drun = " ";
+		drun-display-format = "{name}";
+		window-format = "{w} · {c} · {t}";
+		font = "CaskaydiaCove Nerd Font 10";
+	};
+
+	programs.rofi.theme = lib.mkForce "style.rasi";
+
+	home.file.".config/rofi/style.rasi" = {
+		text = "${colors}${style}";
+		recursive = true;
+	};
+}
