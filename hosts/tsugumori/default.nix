4e5b58bb68c3abafa392b1074197ac8e948677f8
Dhavy Santos <dhavy.s@proton.me>
2024-09-21 01:49:26 -0300

    feat: [tsugumori] setup xserver amdgup + br-abnt configuration

--- hosts/tsugumori/default.nix
+++ hosts/tsugumori/default.nix
@@ -1,7 +1,20 @@
-{ inputs, ... } : {
+{ inputs, pkgs, ... } : {
 	imports = [
 		inputs.sops-nix.nixosModules.sops
 	];
+
+	### XSERVER SECTION
+	services.xserver.videoDrivers = [ "amdgpu" "modesetting" ];
+	services.xserver.xkb.variant = "abnt2";
+	services.xserver.xkb.layout = "br";
+	services.xserver.enable = true;
+
+	hardware.graphics = {
+		extraPackages32 = [ pkgs.driversi686Linux.amdvlk ];
+		extraPackages = [ pkgs.amdvlk ];
+		enable32Bit = true;
+		enable = true;
+	};
 
 	### USERS SECTION
 	users.users.tanikaze = {
