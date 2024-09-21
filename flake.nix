ea7e4d083d2ef58499144890997237700b35bc96
Dhavy Santos <dhavy.s@proton.me>
2024-09-21 01:20:36 -0300

    feat: [flake] add home-manager module

--- flake.nix
+++ flake.nix
@@ -3,9 +3,14 @@
 
 	inputs = {
 		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+		home-manager = {
+			url = "github:nix-community/home-manager";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
 	};
 
-	outputs = { nixpkgs, ... } @inputs : {
+	outputs = { nixpkgs, home-manager, ... } @inputs : {
 		nixosConfigurations.tsugumori = nixpkgs.lib.nixosSystem {
 			specialArgs = { inherit inputs; };
 			modules = [ ./hosts/tsugumori ];
