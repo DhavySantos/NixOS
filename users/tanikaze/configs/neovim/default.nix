445ea609ca0cddeb92a16ae71cd3215da0493054
Dhavy Santos <dhavy.s@proton.me>
2024-09-21 02:23:39 -0300

    fix: [tanikaze] import neovim source using fetchFromGitHub instead flake inputs

--- users/tanikaze/configs/neovim/default.nix
+++ users/tanikaze/configs/neovim/default.nix
@@ -1,12 +1,25 @@
-{ pkgs, inputs, ... } : {
+{ pkgs, ... } :
+
+let
+
+	neovim = pkgs.fetchFromGitHub {
+		sha256 = "L/TX53R+pWmsrx4E1AbDTyWrNubKfSkfNpQtlN6CeN0=";
+		rev = "7c2aec6922e585b150ab354325677268db4a80e6";
+		owner = "DhavySantos";
+		repo = "Neovim";
+	};
+
+in
+
+{
 	home.packages = with pkgs; [ ripgrep ];
 
 	home.file.".config/nvim/lua/tanikaze" = {
-		source = "${inputs.neovim}/lua/tanikaze";
+		source = "${neovim}/lua/tanikaze";
 		recursive = true;
 	};
 
-	stylix.targets.neovim.enable = false;
+	# stylix.targets.neovim.enable = false;
 
 	programs.neovim.extraLuaConfig = ''
 		require("tanikaze.core.options");
