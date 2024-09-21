24316fa1722c1b97a37e4f2b153b278eaf3d5ce2
Dhavy Santos <dhavy.s@proton.me>
2024-09-21 15:06:11 -0300

    feat: [tsugumori] enable sysctl ipv4 forwarding

--- hosts/tsugumori/default.nix
+++ hosts/tsugumori/default.nix
@@ -97,6 +97,8 @@
 	networking.firewall.enable = false;
 	networking.nat.enable = false;
 
+	boot.kernel.sysctl = { "net.ipv4.conf.all.forwarding" = true; };
+
 	services.udev.extraRules = ''
 		SUBSYSTEM=="net", ACTION=="add", ATTR{address}=="18:c0:4d:f4:a7:a6", NAME="wan0"
 		SUBSYSTEM=="net", ACTION=="add", ATTR{address}=="00:e0:4c:68:06:61", NAME="lan0"
