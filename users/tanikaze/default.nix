3e2f7c525772b2d3b4de7caac80cd8beb243f71b
Dhavy Santos <dhavyzhn@gmail.com>
2024-12-20 23:51:44 -0300

    init: [tanikaze] user entrypoint

---
+++ users/tanikaze/default.nix
@@ -0,0 +1,32 @@
+{ inputs, pkgs, ... } : {
+
+  imports = [
+    ./config/mangohud.nix
+    ./config/polybar.nix
+    ./config/stylix.nix
+    ./config/xremap.nix
+    ./config/kitty.nix
+    ./config/i3wm.nix
+    ./config/tmux.nix
+    ./config/zsh.nix
+  ];
+
+  home.homeDirectory = "/home/tanikaze";
+  home.stateVersion = "24.11";
+  home.username = "tanikaze";
+
+  programs.git.extraConfig.user.email = "dhavyzhn@gmail.com"; 
+  programs.git.extraConfig.init.defaultBranch = "maim";
+  programs.git.extraConfig.user.name = "Dhavy Santos"; 
+  programs.git.enable = true;
+
+  programs.lazygit.enable = true;
+  programs.btop.enable = true;
+
+  home.packages = with pkgs; [
+    vesktop google-chrome
+    path-of-building
+  ];
+
+  nixpkgs.config.allowUnfree = true;
+}
