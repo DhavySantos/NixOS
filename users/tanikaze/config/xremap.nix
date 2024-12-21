655fe9d359470e1572754ee1670be5d8d5dcbaa1
Dhavy Santos <dhavyzhn@gmail.com>
2024-12-20 23:50:36 -0300

    init: [tanikaze] xremap configuration

---
+++ users/tanikaze/config/xremap.nix
@@ -0,0 +1,20 @@
+{ inputs, ... } : {
+  imports = [ inputs.xremap.homeManagerModules.default ];
+
+  services.xremap.watch = true;
+  services.xremap.config.keymap = [{
+    name = "default";
+    remap = {
+      Alt-Shift-W = "Shift-7";
+      Capslock = "Esc";
+      Alt-D = "Right";
+      Alt-A = "Left";
+      Alt-S = "Down";
+      Alt-W = "Up";
+    };
+
+    application.not = [
+      "steam_app*"
+    ];
+  }];
+}
