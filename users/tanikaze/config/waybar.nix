457387e024b6960a8964d14fec2491848f6a2e76
Dhavy Santos <dhavyzhn@gmail.com>
2025-01-02 09:04:06 -0300

    init: [tanikaze] basic waybar configuration

---
+++ users/tanikaze/config/waybar.nix
@@ -0,0 +1,42 @@
+{ config, lib, ... } : let
+  colors = with config.lib.stylix.colors; ''
+    @define-color background #${base01};
+    @define-color background-alt #${base02};
+
+    @define-color foreground #${base07};
+    @define-color foreground-alt #${base04};
+    @define-color text #${base07};
+
+    @define-color yellow #${base0A};
+    @define-color blue #${base0D};
+    @define-color red #${base08};
+  '';
+in {
+  programs.waybar.style = lib.mkForce ''${colors}${builtins.readFile ../files/waybar.css}'';
+  programs.waybar.enable = true;
+
+  programs.waybar.settings.main = {
+    position = "bottom";
+    layer = "top";
+    heigh = 24;
+
+    modules-left = [ "custom/system" "hyprland/workspaces" ];
+    modules-center = [ "hyprland/window" ];
+    modules-right = [ "tray" "clock" ];
+
+    "tray" = {
+      icon-size = 16;
+      spacing = 4;
+    };
+
+    "clock" = {
+      interval = 60;
+      format = "{:%H:%M}";
+      tooltip-format = "{:%Y-%m-%s}";
+    };
+
+    "custom/system" = {
+      format = " ";
+    };
+  };
+}
