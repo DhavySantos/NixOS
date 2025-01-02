b2512b1e4a5e57effb03a5e5623c3e856381d37d
Dhavy Santos <dhavyzhn@gmail.com>
2025-01-01 21:48:07 -0300

    feat: [tsugumori] install and configure hyprland service

--- systems/tsugumori/config/display.nix
+++ systems/tsugumori/config/display.nix
@@ -1,15 +1,20 @@
 { pkgs, ... } : {
-  services.displayManager.autoLogin.user = "tanikaze";
-  services.displayManager.autoLogin.enable = true;
-
   systemd.services."autovt@tty1".enable = false;
   systemd.services."getty@tty1".enable = false;
+
+  xdg.portal.extraPortals = with pkgs; [ xdg-desktop-portal-gtk xdg-desktop-portal-hyprland ];
+  xdg.portal.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    dunst libnotify wl-clipboard xclip
+  ];
 
   services.xserver.windowManager.i3.package = pkgs.i3-gaps;
   services.xserver.windowManager.i3.enable = true;
 
-  services.xserver.displayManager.lightdm.enable = true;
-  services.displayManager.defaultSession = "none+i3";
+  services.displayManager.sddm.enable = true;
+  programs.hyprland.xwayland.enable = true;
+  programs.hyprland.enable = true;
 
   services.xserver.videoDrivers = [ "amdgpu" "modesetting" ];
   services.xserver.xkb.variant = "abnt2";
