7bbc0f3d16c38422232611d675a59f28511207a3
Dhavy Santos <dhavyzhn@gmail.com>
2025-01-08 13:58:17 -0300

    refactor: [tanikaze] merge kitty.nix config into shell.nix

--- users/tanikaze/config/kitty.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, ... } : {
-  programs.kitty.enable = true;
-  programs.kitty.settings = {
-    shell = "${pkgs.tmux}/bin/tmux new -A -s Default";
-    font_family = "Cascadia Code NF";
-    confirm_os_window_close = "0";
-    window_margin_width = "10";
-  };
-}
--- users/tanikaze/config/shell.nix
+++ users/tanikaze/config/shell.nix
@@ -5,6 +5,14 @@
 		enable = true;
 	};
 
+  programs.kitty.enable = true;
+  programs.kitty.settings = {
+    shell = "${pkgs.tmux}/bin/tmux new -A -s Default";
+    font_family = "Cascadia Code NF";
+    confirm_os_window_close = "0";
+    window_margin_width = "10";
+  };
+
   programs.zsh = {
     syntaxHighlighting.enable = true;
     autosuggestion.enable = true;
