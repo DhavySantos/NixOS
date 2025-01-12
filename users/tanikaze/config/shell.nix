09ad146334e3b1a258a0e530a6f58f46401b446a
Dhavy Santos <dhavysantos@gmail.com>
2025-01-11 23:09:01 -0300

    fix: [tanikaze] change Cascadia font to Caskaydia

--- users/tanikaze/config/shell.nix
+++ users/tanikaze/config/shell.nix
@@ -8,7 +8,7 @@
   programs.kitty.enable = true;
   programs.kitty.settings = {
     shell = "${pkgs.tmux}/bin/tmux new -A -s Default";
-    font_family = "Cascadia Code NF";
+    font_family = "CaskaydiaCove Nerd Font";
     confirm_os_window_close = "0";
     window_margin_width = "10";
   };
