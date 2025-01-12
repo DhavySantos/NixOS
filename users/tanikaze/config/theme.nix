64b7941fcf013da60776da223af73f05da3da161
Dhavy Santos <dhavysantos@gmail.com>
2025-01-11 23:09:30 -0300

    fix: [tanikaze] set theme polarity to dark

--- users/tanikaze/config/theme.nix
+++ users/tanikaze/config/theme.nix
@@ -1,4 +1,4 @@
-{ pkgs, inputs, ... } : let
+{ pkgs, inputs, lib, ... } : let
   wallpaper = pkgs.fetchurl {
     url = "https://gruvbox-wallpapers.pages.dev/wallpapers/irl/village.jpg";
     hash = "sha256-t3ItqKeewcpGLoyFG4ch23stzGpaujFfANM++Aj3SDM";
@@ -6,6 +6,7 @@
 in {
   imports = [ inputs.stylix.homeManagerModules.stylix ];
   stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-material-dark-medium.yaml";
+  stylix.polarity = "dark";
 
   stylix.cursor.package = pkgs.apple-cursor;
   stylix.cursor.name = "macOS-White";
@@ -14,20 +15,12 @@ in {
   stylix.image = wallpaper;
   stylix.enable = true;
 
-  home.pointerCursor = {
-    package = pkgs.apple_cursor;
-    name = "macOS-White";
-    gtk.enable = true;
-  };
+  stylix.targets.neovim.enable = false;
+
+  gtk.enable = true;
 
   gtk.iconTheme = {
-    package = pkgs.gruvbox-dark-icons-gtk;
+    package = lib.mkForce pkgs.gruvbox-dark-icons-gtk;
     name = "gruvbox-dark";
-  };
-
-  gtk.font = {
-    package = pkgs.cascadia;
-    name = "Cascadia Code NF";
-    size = 12;
   };
 }
