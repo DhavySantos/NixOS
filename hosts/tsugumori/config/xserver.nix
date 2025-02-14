fffa1157ffa17791a60401c059f708aa1c58584d
Dhavy Santos <dhavysantos@gmail.com>
2025-02-14 01:34:29 -0300

    feat: [tsugumori] replace sddm gretter with greetd

--- hosts/tsugumori/config/xserver.nix
+++ hosts/tsugumori/config/xserver.nix
@@ -1,7 +1,26 @@
 { pkgs, ... } : {
-  services.displayManager.autoLogin.user = "tanikaze";
-  services.displayManager.autoLogin.enable = true;
-  services.displayManager.sddm.enable = true;
+  # services.displayManager.autoLogin.user = "tanikaze";
+  # services.displayManager.autoLogin.enable = true;
+  # services.displayManager.sddm.enable = true;
+
+  services.greetd = {
+    enable = true;
+    settings.default_session = {
+      command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland";
+      user = "tanikaze";
+    };
+  };
+
+  systemd.services.greetd.serviceConfig = {
+    Type = "idle";
+    StandardInput = "tty";
+    StandardOutput = "tty";
+    StandardError = "journal";
+
+    TTYReset = true;
+    TTYVHangup = true;
+    TTYVTDisallocate = true;
+  };
 
   systemd.services."autovt@tty1".enable = false;
   systemd.services."getty@tty1".enable = false;
