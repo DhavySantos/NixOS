223d23543afa0b419d0f8f9b19fe05eeedd2c2c1
Dhavy Santos <dhavysantos@gmail.com>
2025-02-20 02:48:05 -0300

    refactor: [tsugumori] docker configuration

--- hosts/tsugumori/config/docker/default.nix
+++ hosts/tsugumori/config/docker/default.nix
@@ -1,36 +1,15 @@
-{ pkgs, ... } : {
-  virtualisation.docker.daemon.settings.bip = "172.17.0.1/16";
-  virtualisation.docker.daemon.settings.iptables = false;
+{ ... } : {
   virtualisation.oci-containers.backend = "docker";
   virtualisation.docker.enable = true;
 
-  systemd.services."network-docker1" = {
-    serviceConfig.Type = "oneshot";
-
-    after = [ "docker.service" ];
-
-    wantedBy = [
-      "docker-sonarr.service"
-      "docker-radarr.service"
-      "docker-jellyfin.service"
-      "docker-prowlarr.service"
-      "docker-flaresolverr.service"
-      "docker-transmission.service"
-    ];
-
-    script = ''
-      ${pkgs.docker}/bin/docker network inspect docker1 > /dev/null 2>&1 || ${pkgs.docker}/bin/docker network create \
-      --opt com.docker.network.bridge.name=docker1 \
-      --subnet 172.18.0.0/16 docker1
-    '';
-  };
-
   virtualisation.oci-containers.containers = {
-    jellyfin = import ./containers/jellyfin.nix; # 172.18.1.1
-    sonarr = import ./containers/sonarr.nix; # 172.18.1.2
-    radarr = import ./containers/radarr.nix; # 172.18.1.3
-    prowlarr = import ./containers/prowlarr.nix; # 172.18.1.4
-    flaresolverr = import ./containers/flaresolverr.nix; # 172.18.1.5
-    transmission = import ./containers/transmission.nix; # 172.18.1.6
+    sonarr = import ./containers/sonarr.nix;
+    radarr = import ./containers/radarr.nix;
+    prowlarr = import ./containers/prowlarr.nix;
+    jellyfin = import ./containers/jellyfin.nix;
+    transmission = import ./containers/transmission.nix;
+    flaresolverr = import ./containers/flaresolverr.nix;
+
+    minecraft = import ./containers/minecraft.nix;
   };
 }
