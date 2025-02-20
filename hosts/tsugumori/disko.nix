0f31c3033d3c155b55141fa09906e8f9381eea76
Dhavy Santos <dhavysantos@gmail.com>
2025-02-20 03:01:38 -0300

    feat: [tsugumori] setup disko configuration

---
+++ hosts/tsugumori/disko.nix
@@ -0,0 +1,23 @@
+{
+  disko.devices.disk.main = {
+    device = "/dev/disk/by-id/nvme-NVME_SSD_512GB_GSMD22A0900736-part1";
+    content.type = "gpt";
+    type = "disk";
+
+    content.partitions.ESP = {
+      type = "EF00";
+      size = "512M";
+      content.format = "vfat";
+      content.type = "filesystem";
+      content.mountpoint = "/boot";
+      content.mountOptions = [ "umask=0077" ];
+    };
+
+    content.partitions.ROOT = {
+      size = "100%";
+      content.format = "ext4";
+      cnotent.mountpoint = "/";
+      content.type = "filesystem";
+    };
+  };
+}
