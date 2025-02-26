aa7dc3f0fed85a8689c2c6807319fd4223da5cb5
Dhavy Santos <dhavysantos@gmail.com>
2025-02-26 12:00:13 -0300

    remove: [tsugumori] replace greetd by sddm with autologin

--- hosts/tsugumori/config/xserver.nix
+++ hosts/tsugumori/config/xserver.nix
@@ -1,26 +1,7 @@
-{ pkgs, ... } : {
-  # services.displayManager.autoLogin.user = "tanikaze";
-  # services.displayManager.autoLogin.enable = true;
-  # services.displayManager.sddm.enable = true;
-
-  services.greetd = {
-    enable = true;
-    settings.default_session = {
-      command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland";
-      user = "tanikaze";
-    };
-  };
-
-  systemd.services.greetd.serviceConfig = {
-    Type = "idle";
-    StandardInput = "tty";
-    StandardOutput = "tty";
-    StandardError = "journal";
-
-    TTYReset = true;
-    TTYVHangup = true;
-    TTYVTDisallocate = true;
-  };
+{
+  services.displayManager.autoLogin.user = "tanikaze";
+  services.displayManager.autoLogin.enable = true;
+  services.displayManager.sddm.enable = true;
 
   systemd.services."autovt@tty1".enable = false;
   systemd.services."getty@tty1".enable = false;
