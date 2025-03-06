69c1d7b9ccefe1aab343c7f1b46c95a950d3b8c2
Dhavy Santos <dhavysantos@gmail.com>
2025-03-06 11:46:22 -0300

    refactor: [tanikaze] remove redundant let in scope

--- users/tanikaze/config/theme.nix
+++ users/tanikaze/config/theme.nix
@@ -1,20 +1,17 @@
-{ pkgs, inputs, ... } : let
+{ inputs, pkgs, ... }:
 
-wallpaper = pkgs.fetchurl {
-  url = "https://i.imgur.com/5ALLimu.jpeg";
-  hash = "sha256-7/lzW5BfpC1yF1NwOXnTZ/+IXMavK+Vd71bYQBfUqEA=";
-};
-
-in {
+{
   imports = [ inputs.stylix.homeManagerModules.stylix ];
   stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
   stylix.polarity = "dark";
+  stylix.enable = true;
 
   stylix.cursor.package = pkgs.apple-cursor;
   stylix.cursor.name = "macOS-White";
   stylix.cursor.size = 24;
 
-  stylix.targets.neovim.enable = false;
-  stylix.image = wallpaper;
-  stylix.enable = true;
+  stylix.image = pkgs.fetchurl {
+    url = "https://i.imgur.com/5ALLimu.jpeg";
+    hash = "sha256-7/lzW5BfpC1yF1NwOXnTZ/+IXMavK+Vd71bYQBfUqEA=";
+  };
 }
