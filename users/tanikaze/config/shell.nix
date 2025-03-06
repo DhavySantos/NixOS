3458f1963bf3d483655fe82f5d04e1696465cd43
Dhavy Santos <dhavysantos@gmail.com>
2025-03-06 11:45:37 -0300

    refactor: [tanikaze] move tmux cfg file into the nix

--- users/tanikaze/config/shell.nix
+++ users/tanikaze/config/shell.nix
@@ -1,19 +1,40 @@
-{ pkgs, config, lib, ... } : {
-  programs.tmux = {
-		extraConfig = builtins.readFile ../files/tmux.conf;
-		shell = "${pkgs.zsh}/bin/zsh";
-		enable = true;
-	};
+{ config, pkgs, ... }:
+
+{
+  programs.tmux.shell = "${pkgs.zsh}/bin/zsh";
+  programs.tmux.enable = true;
+  programs.tmux.extraConfig = ''
+    # Set Options
+    set -g default-terminal "screen-256color"
+    set -g terminal-overrides "xterm*:Tc"
+    set -g detach-on-destroy off
+    set -g mouse on
+
+    # Create and Switch Between Windows
+    bind -n M-t new-window -c "#{pane_current_path}"
+    bind -n M-q previous-window
+    bind -n M-e next-window
+    bind -n M-k killp
+
+    # Create and Swtich Between Sessions
+    bind -n M-T new-session -c "#{pane_current_path}"
+    bind -n M-Q switch-client -p
+    bind -n M-E switch-client -n
+    bind -n M-K kill-session
+
+    # Set To Rename to Current Path
+    set-option -g automatic-rename-format "#{b:pane_current_path}"
+    set-option -g automatic-rename on
+    set-option -g status-interval 1
+  '';
 
   programs.kitty.enable = true;
   programs.kitty.settings = {
     shell = "${pkgs.tmux}/bin/tmux new -A -s default";
     font_family = "CaskaydiaCove Nerd Font";
-    confirm_os_window_close = "0";
-    window_margin_width = "10";
-    background_opacity = lib.mkForce 0.9;
-    background_blur = 10;
-    font_size = 10.0;
+    confirm_os_window_close = 0;
+    window_margin_width = 10;
+    font_size = 10;
   };
 
   programs.direnv.enableZshIntegration = true;
