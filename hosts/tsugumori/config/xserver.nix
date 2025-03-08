b66366a4a6e3a85acbb385c2c32fa263bdbffa53
Dhavy Santos <dhavyzhn@gmail.com>
2025-03-07 22:30:19 -0300

    feat: [tsugumori] replace hyprland by i3wm

--- hosts/tsugumori/config/xserver.nix
+++ hosts/tsugumori/config/xserver.nix
@@ -1,13 +1,16 @@
 {
   services.displayManager.autoLogin.user = "tanikaze";
   services.displayManager.autoLogin.enable = true;
-  services.displayManager.sddm.enable = true;
+  services.displayManager.lightdm.enable = true;
+
+  services.xserver.displayManager.defaultSession = "nonde+i3";
+  services.xserver.desktopManager.xterm.enable = false;
+  services.xserver.windowManager.i3.enable = true;
 
   systemd.services."autovt@tty1".enable = false;
   systemd.services."getty@tty1".enable = false;
 
-  programs.hyprland.xwayland.enable = true;
-  programs.hyprland.enable = true;
+  environment.pathsToLink = [ "/libexec" ];
 
   services.xserver.videoDrivers = [ "amdgpu" ];
   services.xserver.xkb.variant = "abnt2";
