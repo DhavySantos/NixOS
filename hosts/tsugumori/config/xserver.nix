bd89236c3a5c688ce4390f52e014abda6f5db05e
Dhavy Santos <DhavyZhn@gmail.com>
2025-03-12 14:43:20 -0300

    feat: [tsugumori] set custom resolution on xorg service

--- hosts/tsugumori/config/xserver.nix
+++ hosts/tsugumori/config/xserver.nix
@@ -1,9 +1,18 @@
+{ pkgs, ... }:
+
 {
   services.displayManager.autoLogin.user = "tanikaze";
+  services.displayManager.defaultSession = "none+i3";
   services.displayManager.autoLogin.enable = true;
-  services.displayManager.lightdm.enable = true;
 
-  services.xserver.displayManager.defaultSession = "nonde+i3";
+  services.xserver.displayManager.lightdm.enable = true;
+  services.xserver.displayManager.setupCommands = ''
+    ${pkgs.xorg.xrandr}/bin/xrandr --newmode "1920x1080R" 138.50 1920 1968 2000 2080 1080 1083 1088 1111 +hsync -vsync
+    ${pkgs.xorg.xrandr}/bin/xrandr --addmode HDMI-A-1 "1920x1080R"
+    ${pkgs.xorg.xrandr}/bin/xrandr --output HDMI-A-1 --mode "1920x1080R"
+    ${pkgs.xorg.xrandr}/bin/xrandr --output HDMI-A-0 --primary --mode "1920x1080" --below HDMI-A-1
+  '';
+
   services.xserver.desktopManager.xterm.enable = false;
   services.xserver.windowManager.i3.enable = true;
 
