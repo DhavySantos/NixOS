5c090474fcc5bbe74a07d785d4d3997a82a049cc
Dhavy Santos <DhavyZhn@gmail.com>
2025-03-13 00:32:17 -0300

    fix: [tsugumori] add v4l2loop module to suppoer droidcam

--- hosts/tsugumori/hardware.nix
+++ hosts/tsugumori/hardware.nix
@@ -8,8 +8,13 @@
   boot.kernelModules = [ "kvm-intel" "v4l2loopback" ];
   boot.initrd.kernelModules = [ "amdgpu" ];
   boot.supportedFilesystems = [ "btrfs" ];
+
+  boot.extraModulePackages = with config.boot.kernelPackages; [
+    v4l2loopback
+  ];
+
   boot.extraModprobeConfig = ''
-    options v4l2loopback devices=1 video_nr=1 card_label="Droidcam" exclusive_caps=1
+    options v4l2loopback video_nr=2,3 width=640,1920 max_width=1920 height=480,1080 max_height=1080 format=YU12,YU12 exclusive_caps=1,1 card_label=Phone,Laptop debug=1
   '';
 
   fileSystems."/boot" = lib.mkForce {
