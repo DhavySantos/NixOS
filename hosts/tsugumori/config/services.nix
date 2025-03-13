eaeba7e1a0bc876c3869618202c9d4d7ca49dbc4
Dhavy Santos <DhavyZhn@gmail.com>
2025-03-13 00:31:43 -0300

    feat: [tsugumori] setup cpupower service to set frequency to boost

--- hosts/tsugumori/config/services.nix
+++ hosts/tsugumori/config/services.nix
@@ -9,6 +9,17 @@
     enable = true;
   };
 
+  systemd.services.cpupower = {
+    description = "Set CPU performance level";
+    wantedBy = [ "multi-user.target" ];
+    after = [ "network.target" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.linuxPackages_latest.cpupower}/bin/cpupower frequency-set -d 4.1GHz";
+      RemainAfterExit = true;
+      Type = "oneshot";
+    };
+  };
+
   services.openssh = {
     enable = true;
     settings = {
