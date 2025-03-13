fde17ffeadaee308146f4a8713ed0c106f43dfd3
Dhavy Santos <DhavyZhn@gmail.com>
2025-03-13 01:14:46 -0300

    fix: [tsugumori] replace native filesystem configuration with disko module

--- hosts/tsugumori/hardware.nix
+++ hosts/tsugumori/hardware.nix
@@ -17,28 +17,6 @@
     options v4l2loopback video_nr=2,3 width=640,1920 max_width=1920 height=480,1080 max_height=1080 format=YU12,YU12 exclusive_caps=1,1 card_label=Phone,Laptop debug=1
   '';
 
-  fileSystems."/boot" = lib.mkForce {
-    device = "/dev/disk/by-id/nvme-NVME_SSD_512GB_GSMD22A0900736-part1";
-    options = [ "fmask=0022" "dmask=0022" ];
-    fsType = "vfat";
-  };
-
-  fileSystems."/" = lib.mkForce {
-    device = "/dev/mapper/root";
-    fsType = "ext4";
-  };
-
-  fileSystems."/mnt/steam" = lib.mkForce {
-    device = "/dev/disk/by-id/ata-Hitachi_HTS547550A9E384_J2260056GD9SXC-part1";
-    options = [ "nofail" "subvol=@steam" "compress=zstd" ];
-    fsType = "btrfs";
-  };
-
-  boot.initrd.luks.devices."root" = {
-    device = "/dev/disk/by-id/nvme-NVME_SSD_512GB_GSMD22A0900736-part2";
-    preLVM = true;
-  };
-
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
   boot.kernelPackages = pkgs.linuxPackages_latest;
   nixpkgs.hostPlatform = lib.mkDefault system;
